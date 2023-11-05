4b387e2ef4edd4ca05f4190aae52c3c00aba74f2
Maria Golovatenko <pikch4y@yandex.com>
2023-11-05 12:42:04 +0300

    feat: добавить стили активному элементу

--- src/entities/categories-list/index.tsx
+++ src/entities/categories-list/index.tsx
@@ -1,31 +1,36 @@
 import Category from '../category';
 import useCategoriesStore from '../../shared/storages/categories';
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
 
 function CategoriesList({
   setCurrentItems,
   products
 }: {
-  setCurrentItems: Dispatch<SetStateAction<object[]>>;
+  setCurrentItems: Dispatch<SetStateAction<any>>;
   products: Array<object>;
 }) {
   const categories = useCategoriesStore(store => store.categories);
+  const [active, setActive] = useState('1');
 
-  function chooseCategory(category: string) {
+  function chooseCategory(category: string, id: string) {
     if (category === 'Все товары') {
       setCurrentItems(products);
+      setActive(id);
       return;
     }
 
     setCurrentItems(products.filter((el: object) => el.category === category));
+    setActive(id);
   }
   return (
     <ul className="flex flex-col gap-2">
       {categories.map(category => (
         <Category
           key={category.id}
+          id={category.id}
           title={category.value}
-          handleClick={() => chooseCategory(category.value)}
+          handleClick={() => chooseCategory(category.value, category.id)}
+          active={active}
         />
       ))}
     </ul>
--- src/entities/category/index.tsx
+++ src/entities/category/index.tsx
@@ -1,9 +1,23 @@
 import { MouseEventHandler } from 'react';
 
-function Category({ title, handleClick }: { title: string; handleClick: MouseEventHandler }) {
+function Category({
+  title,
+  handleClick,
+  active,
+  id
+}: {
+  title: string;
+  handleClick: MouseEventHandler;
+  active: string;
+  id: string;
+}) {
   return (
     <li
-      className="list-none p-3 bg-light rounded-lg w-48 text-base hover:opacity-70 hover:cursor-pointer"
+      className={
+        active === id
+          ? 'list-none p-3 bg-blue rounded-lg w-48 text-base text-white hover:opacity-70 hover:cursor-pointer'
+          : 'list-none p-3 bg-light rounded-lg w-48 text-base hover:opacity-70 hover:cursor-pointer'
+      }
       onClick={handleClick}
     >
       {title}
